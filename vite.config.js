00876cea08a316f372ff459000f2e84c02ffde04
Patrik <airapetov2002@gmail.com>
2025-10-24 17:13:16 +0200

    work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// ЗАМЕНИ <REPO_NAME> на имя твоего репозитория
 export default defineConfig({
   plugins: [vue()],
-  base: '/<REPO_NAME>/',   // например '/drum-visualizer-vue/'
 })
